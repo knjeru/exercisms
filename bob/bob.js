5e33e3a023eaa481fafb05d4c37b3996715c80c9
Kevin Njeru <knjeru@gmail.com>
2016-02-11 13:51:02 -0700

    Conditionals prior num tests are working

--- bob/bob.js
+++ bob/bob.js
@@ -6,10 +6,35 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
-
+  console.log(input);
+  var nums = [0,1,2,3,4,5,6,7,8,9];
+  var isShout = input.toUpperCase();
+  var numRegEx = /[0-9]/gi;
+  var sil1 = 32;
+  var sil2 = '';
+  for (var i = 0; i < input.length; i++) {
+    if (input.slice(-1) === sil2 || input.charCodeAt(i) === sil1) {
+      return 'Fine. Be that way!';
+    }
+    if (input.slice(-1) === '.') {
+      return 'Whatever.';
+    }
+    if (numRegEx === input[i]) {
+      return 'Whatever.';
+    }
+    if (isShout === input) {
+      return 'Whoa, chill out!';
+    }
+    if (input.slice(-1) === '?') {
+      return 'Sure.';
+    }
+    if (input.slice(-1) === '!') {
+      return 'Whatever.';
+    }
+    else {
+      return 'Whatever.';
+    }
+  }
 };
 
 module.exports = Bob;
